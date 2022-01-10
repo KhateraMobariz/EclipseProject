60211f7fea4f6e7ff32ac7e8af9d97111e2221e5
KhateraMobariz <khatera.mobariz1990@gmail.com>
2022-01-10 12:45:27 -0500

    test

--- src/GitProject.java
+++ src/GitProject.java
@@ -4,6 +4,7 @@ public class GitProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Selenium is dificult");
+	}
 		
 		public void methodA(){
 			
@@ -16,4 +17,4 @@ public class GitProject {
 
 	}
 
-}
+
