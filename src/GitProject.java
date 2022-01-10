306e1e9d2ee62ac435f26a483e0e928044186d78
KhateraMobariz <97072577+KhateraMobariz@users.noreply.github.com>
2022-01-10 12:28:15 -0500

    Update GitProject.java
    Remote change

--- src/GitProject.java
+++ src/GitProject.java
@@ -4,6 +4,15 @@ public class GitProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Selenium is dificult");
+		
+		public void methodA(){
+			
+			// this is from remote repositry
+			
+			
+		}
+		
+		
 
 	}
 
